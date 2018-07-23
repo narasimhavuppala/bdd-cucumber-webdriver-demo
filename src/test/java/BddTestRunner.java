331ea6b5a223179cdf9096a4a2de850234b6cc10
Karthik Reddy <bobbili.kr@gmail.com>
2018-07-23 10:56:54 +0530

    Added jenkins options

--- src/test/java/BddTestRunner.java
+++ src/test/java/BddTestRunner.java
@@ -1,8 +1,10 @@
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin= {"pretty","html:target/cucumber", "json:target/cucumber.json"})
 public class BddTestRunner {
 
 }
