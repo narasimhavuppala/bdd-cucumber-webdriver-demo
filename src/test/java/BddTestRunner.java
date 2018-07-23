e6a302daf49edc5620983c3ead309c7633fda469
Karthik Reddy <bobbili.kr@gmail.com>
2018-07-23 11:09:24 +0530

    first commit

--- src/test/java/BddTestRunner.java
+++ src/test/java/BddTestRunner.java
@@ -4,7 +4,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin= {"pretty","html:target/cucumber", "json:target/cucumber.json"})
+@CucumberOptions(plugin= {"pretty","html:target/cucumber", "json:target/cucumber.json"},
+tags= {"@HL"})
 public class BddTestRunner {
 
 }
